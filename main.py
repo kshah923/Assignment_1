683972b9f9dcab666b5ef7353322e985bbd2c9f4
Kevin <k.shah923@gmail.com>
2021-02-20 12:09:44 -0500

    As of 12:09PM
    No Tan

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ import matplotlib.pyplot as plt
 time = np.arange(0, 2*np.pi, 0.1)
 sin = np.sin(time)
 cos = np.cos(time)
-tan = np.tan(time)
 plt.plot(time, sin, time, cos)
 
 # title, legend, color_coding
