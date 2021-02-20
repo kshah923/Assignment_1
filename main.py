15de1dc75dd65a080c51d60142d3301636b2c9fc
Kevin <k.shah923@gmail.com>
2021-02-20 11:51:59 -0500

    No_tan
    no tangent

--- main.py
+++ main.py
@@ -11,14 +11,11 @@ sin = np.sin(time)
 cos = np.cos(time)
 tan = np.tan(time)
 plt.plot(time, sin, time, cos)
-# plt.plot(time, sin, time, cos, time, tan)
 
-# title, legend, color_coding,
+# title, legend, color_coding
 plt.title('Trig Graphs')
 plt.xlabel('Time')
 plt.ylabel('Amplitude')
 plt.axhline(linewidth=1, color='black')
 plt.legend(['sin(x)', 'cos(x)'])
-# plt.legend(['sin(x)', 'cos(x)', 'tan(x)'])
-# plt.ylim([-1.5, 1.5])
 plt.show()
